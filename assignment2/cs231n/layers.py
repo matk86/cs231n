complete rnn in assignmnet 3, attempt to fix batch norm backprop in assignmnet 2 to no avail

assignment2/cs231n/layers.py
@@ -190,11 +190,12 @@ def batchnorm_backward(dout, cache):
   dgamma = np.sum(dout * xhat, axis=0)
   # wrt x
   dxhat = dout * gamma
-  dvar = -np.sum(dxhat * xhat1, axis=0) / (std**2)
+  dvar = -np.sum(dxhat * xhat1 / (std**2), axis=0)
   dvar2 = dvar /2./std
   #dmub = - np.sum(dxhat / std, axis=0) - 2 * dvar * np.mean(xhat1, axis=0)
   #dmub = - np.sum(dxhat / std + 2 * dvar * xhat1, axis=0)
-  dmub = - np.sum(dxhat / std, axis=0)  
+  dmub = - np.sum(dxhat / std + 2 * dvar2 * xhat1/N, axis=0)  
+  #dmub = - np.sum(dxhat / std, axis=0)  
   dx = dxhat/std + 2 * dvar2 * xhat1 / N + dmub / N
 
   return dx, dgamma, dbeta  # dx not correct!!

assignment3/cs231n/classifiers/rnn.py
@@ -47,26 +47,26 @@ class CaptioningRNN(object):
     self._end = word_to_idx.get('<END>', None)
     
     # Initialize word vectors
-    self.params['W_embed'] = np.random.randn(vocab_size, wordvec_dim)
+    self.params['W_embed'] = np.random.randn(vocab_size, wordvec_dim)  # V, W
     self.params['W_embed'] /= 100
     
     # Initialize CNN -> hidden state projection parameters
-    self.params['W_proj'] = np.random.randn(input_dim, hidden_dim)
+    self.params['W_proj'] = np.random.randn(input_dim, hidden_dim)  # D, H
     self.params['W_proj'] /= np.sqrt(input_dim)
     self.params['b_proj'] = np.zeros(hidden_dim)
 
     # Initialize parameters for the RNN
     dim_mul = {'lstm': 4, 'rnn': 1}[cell_type]
-    self.params['Wx'] = np.random.randn(wordvec_dim, dim_mul * hidden_dim)
+    self.params['Wx'] = np.random.randn(wordvec_dim, dim_mul * hidden_dim)  # W, H
     self.params['Wx'] /= np.sqrt(wordvec_dim)
-    self.params['Wh'] = np.random.randn(hidden_dim, dim_mul * hidden_dim)
+    self.params['Wh'] = np.random.randn(hidden_dim, dim_mul * hidden_dim)  # H, H
     self.params['Wh'] /= np.sqrt(hidden_dim)
-    self.params['b'] = np.zeros(dim_mul * hidden_dim)
+    self.params['b'] = np.zeros(dim_mul * hidden_dim)  # H
     
     # Initialize output to vocab weights
-    self.params['W_vocab'] = np.random.randn(hidden_dim, vocab_size)
+    self.params['W_vocab'] = np.random.randn(hidden_dim, vocab_size)  # H, V
     self.params['W_vocab'] /= np.sqrt(hidden_dim)
-    self.params['b_vocab'] = np.zeros(vocab_size)
+    self.params['b_vocab'] = np.zeros(vocab_size)  # V
       
     # Cast parameters to correct dtype
     for k, v in self.params.iteritems():
@@ -114,8 +114,27 @@ class CaptioningRNN(object):
     W_vocab, b_vocab = self.params['W_vocab'], self.params['b_vocab']
     
     loss, grads = 0.0, {}
+
+    image_affine, i_cache = affine_forward(features, W_proj, b_proj)  # N, H
+    captions_affine, c_cache = word_embedding_forward(captions_in, W_embed)  # N, T-1, D
+
+    if self.cell_type == "rnn":
+      h, h_cache = rnn_forward(captions_affine, image_affine, Wx, Wh, b)  # N, T-1, H
+
+    scores, s_cache = temporal_affine_forward(h, W_vocab, b_vocab)  # N, T-1, V
+
+    loss, dLdscores = temporal_softmax_loss(scores, captions_out, mask)
+
+
+    # back prop
+    dout_rnn, grads["W_vocab"], grads["b_vocab"] = temporal_affine_backward(dLdscores, s_cache)
+    dx_rnn, dh0, grads["Wx"], grads["Wh"], grads["b"] = rnn_backward(dout_rnn, h_cache)
+    # image backprop
+    dx_i, grads["W_proj"], grads["b_proj"] = affine_backward(dh0, i_cache)
+    # word embed backprop
+    grads["W_embed"] = word_embedding_backward(dx_rnn, c_cache)
     ############################################################################
-    # TODO: Implement the forward and backward passes for the CaptioningRNN.   #
+    #
     # In the forward pass you will need to do the following:                   #
     # (1) Use an affine transformation to compute the initial hidden state     #
     #     from the image features. This should produce an array of shape (N, H)#
@@ -134,10 +153,6 @@ class CaptioningRNN(object):
     # with respect to all model parameters. Use the loss and grads variables   #
     # defined above to store loss and gradients; grads[k] should give the      #
     # gradients for self.params[k].                                            #
-    ############################################################################
-    pass
-    ############################################################################
-    #                             END OF YOUR CODE                             #
     ############################################################################
     
     return loss, grads
@@ -175,9 +190,20 @@ class CaptioningRNN(object):
     W_embed = self.params['W_embed']
     Wx, Wh, b = self.params['Wx'], self.params['Wh'], self.params['b']
     W_vocab, b_vocab = self.params['W_vocab'], self.params['b_vocab']
-    
+
+
+    for n in xrange(N):
+      prev_h, _ = affine_forward(features[n, :].reshape(1, features.shape[1]), W_proj, b_proj)  # 1, H
+      word_t = self._start
+
+      for t in xrange(max_length):
+        captions[n, t] = word_t
+        x = W_embed[word_t].reshape(1, W_embed.shape[1])  # 1, W
+        next_h, _ = rnn_step_forward(x, prev_h, Wx, Wh, b)  # 1, H
+        score = next_h.dot(W_vocab) + b_vocab.reshape((1, b_vocab.shape[0]))  # 1, V
+        word_t = np.argmax(score)
     ###########################################################################
-    # TODO: Implement test-time sampling for the model. You will need to      #
+    #  You will need to
     # initialize the hidden state of the RNN by applying the learned affine   #
     # transform to the input image features. The first word that you feed to  #
     # the RNN should be the <START> token; its value is stored in the         #
@@ -197,8 +223,4 @@ class CaptioningRNN(object):
     # functions; you'll need to call rnn_step_forward or lstm_step_forward in #
     # a loop.                                                                 #
     ###########################################################################
-    pass
-    ############################################################################
-    #                             END OF YOUR CODE                             #
-    ############################################################################
     return captions

assignment3/cs231n/rnn_layers.py
@@ -26,6 +26,7 @@ def rnn_step_forward(x, prev_h, Wx, Wh, b):
   - next_h: Next hidden state, of shape (N, H)
   - cache: Tuple of values needed for the backward pass.
   """
+  # Ht = Wx X + Wh Ht-1 + b
   next_h = np.tanh(x.dot(Wx) + prev_h.dot(Wh) + b)
   cache = (x, prev_h, Wx, Wh, next_h)
   return next_h, cache
@@ -75,18 +76,17 @@ def rnn_forward(x, h0, Wx, Wh, b):
   - h: Hidden states for the entire timeseries, of shape (N, T, H).
   - cache: Values needed in the backward pass
   """
-  h, cache = None, None
   N, T, D = x.shape
   H = h0.shape[1]
   h = np.zeros((N,T, H))
 
-  for t in range(0, T):
+  for t in xrange(0, T):
     if t==0:
       h[:, 0, :], _ = rnn_step_forward(x[:, 0, :], h0, Wx, Wh, b)  # h0
     else:
       h[:, t, :], _ = rnn_step_forward(x[:, t, :], h[:, t-1, :], Wx, Wh, b)
 
-  cache = (x, h, Wx, Wh)
+  cache = (x, h0, h, Wx, Wh)
   return h, cache
 
 
@@ -104,21 +104,31 @@ def rnn_backward(dh, cache):
   - dWh: Gradient of hidden-to-hidden weights, of shape (H, H)
   - db: Gradient of biases, of shape (H,)
   """
-  dx, dh0, dWx, dWh, db = None, None, None, None, None
-  x, h, Wx, Wh  = cache
+  # Ht = Wx X + Wh Ht-1 + b
+  # dHt/dx = Wx, dHt/dWx = X, dHt/dWh = Ht-1, dHt/db = 1
+  # dHt/dHt-1 = Wh
+  x, h0, h, Wx, Wh  = cache
   N, T, D = x.shape
   H = h.shape[2]
   dx = np.zeros((N, T, D))
+  dhprev = np.zeros((N, H))
+  dWx = np.zeros((D, H))
+  dWh = np.zeros((H, H))
+  db = np.zeros((H,))
 
-  for t in range(T-1, -1, -1): # t-1 .. 0
+  for t in xrange(T-1, -1, -1): # t-1 .. 0
     if t==0:
-
+      cache_t = (x[:, t, :], h0, Wx, Wh, h[:, t, :])
     else:
       cache_t = (x[:, t, :], h[:, t-1, :], Wx, Wh, h[:, t, :])
-      dx[:, t, :], dprev_h, dWx, dWh, db = rnn_step_backward(dh[:, t, :], cache_t)
+    # h has 2 connections: one out and one to next timestep, hence the sum
+    dx[:, t, :], dhprev, dWxt, dWht, dbt = rnn_step_backward(dh[:, t, :] + dhprev, cache_t)
+    # Wx, Wh and b the same for all t, so add up
+    dWx += dWxt
+    dWh += dWht
+    db += dbt
 
-
-  return dx, dh0, dWx, dWh, db
+  return dx, dhprev, dWx, dWh, db
 
 
 def word_embedding_forward(x, W):
@@ -136,16 +146,10 @@ def word_embedding_forward(x, W):
   - out: Array of shape (N, T, D) giving word vectors for all input words.
   - cache: Values needed for the backward pass
   """
-  out, cache = None, None
-  ##############################################################################
-  # TODO: Implement the forward pass for word embeddings.                      #
-  #                                                                            #
-  # HINT: This should be very simple.                                          #
-  ##############################################################################
-  pass
-  ##############################################################################
-  #                               END OF YOUR CODE                             #
-  ##############################################################################
+  N, T = x.shape
+  V, D = W.shape
+  out = W[x.ravel()].reshape((N, T, D))
+  cache = (x, W)
   return out, cache
 
 
@@ -164,16 +168,14 @@ def word_embedding_backward(dout, cache):
   Returns:
   - dW: Gradient of word embedding matrix, of shape (V, D).
   """
-  dW = None
-  ##############################################################################
-  # TODO: Implement the backward pass for word embeddings.                     #
-  #                                                                            #
-  # HINT: Look up the function np.add.at                                       #
-  ##############################################################################
-  pass
-  ##############################################################################
-  #                               END OF YOUR CODE                             #
-  ##############################################################################
+  x, W = cache
+  N, T = x.shape
+  V, D = W.shape
+  dW = np.zeros((V,D))
+  for n in xrange(N):
+    for t in xrange(T):
+      dW[x[n,t], :] += dout[n,t, :]
+  #np.add.at(dW, x, dout)
   return dW
 
 